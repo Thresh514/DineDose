4a5a7e0d2ab42af1de3f8c93bc25415f22521c0b
sky <skyjin@bu.edu>
2025-10-28 17:09:22 -0400

    created file for doctor_dashboard.html and its corresponding render logic

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 
-from pagelogic import index
+from pagelogic import (index, doctor_dashboard)
+
 
 app = Flask(__name__)
 
 @app.route('/' , methods=['GET','POST'])
 def index_page():
     return index.index()
+
+@app.route('/doctor', methods=['GET', 'POST'])
+def goto_doctor_dashboard():
+    return doctor_dashboard.index()
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
