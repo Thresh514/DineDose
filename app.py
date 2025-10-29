c492880bd06e430dabe34c2bc98ee09d283529ea
sky <skyjin@bu.edu>
2025-10-29 15:19:08 -0400

    add basic logic to access user in db, will add hanlder in the next step

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
+from pagelogic import signup
+from repository.db import db_init
 from pagelogic import (index, doctor_dashboard)
 
 
 app = Flask(__name__)
+db = db_init(app) #initialize the connection to the database
 
 @app.route('/' , methods=['GET','POST'])
 def index_page():
@@ -12,6 +15,16 @@ def index_page():
 @app.route('/doctor', methods=['GET', 'POST'])
 def goto_doctor_dashboard():
     return doctor_dashboard.index()
+
+@app.route('/doctor/signup', methods=['POST'])
+def create_doctor():
+    data = request.get_json()  # 从 body 中获取 JSON 数据
+    username = data.get('username')
+    email = data.get('email')
+    return signup.doctorSignUp(username, email)
+
+
+
 
 
 
