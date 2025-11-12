99e0cb1a202e84f46d980915b62458654753cc4c
Dingwen1125 <laoba@bu.edu>
2025-11-12 17:43:43 -0500

    update plan_repo

--- pagelogic/plan_service.py
+++ pagelogic/plan_service.py
@@ -3,47 +3,47 @@
 from pagelogic.repo import plan_repo
 
 
-# def GetUserPlan(
-#             id, #获取谁的Plan
-#             frofrom_when, #从这个时间
-#             to,   #到这个时间
-#                 ): 
-#     plan = plan_repo.get_plan_by_user_id(id) 
-#     #获取用户对应的plan, assume每个用户目前只有一个plan
+def GetUserPlan(
+            id, #获取谁的Plan
+            frofrom_when, #从这个时间
+            to,   #到这个时间
+                ): 
+    plan = plan_repo.get_plan_by_user_id(id) 
+    #获取用户对应的plan, assume每个用户目前只有一个plan
     
-#     plan_items = get_all_plan_items_by_plan_id(plan.id)
-#     #获取每个plan_item
+    plan_items = get_all_plan_items_by_plan_id(plan.id)
+    #获取每个plan_item
     
-#     item_ids_to_rules = plan_repo.get_plan_item_rules_by_plan_id(plan.id)
-#     #获取每个plan_item对应的plan_item_rule
-#     #item_ids_to_rules 是一个dictionary，
-#     #Key: plan_item.id
-#     #Value: 对应的 plan_item_rules
+    item_ids_to_rules = plan_repo.get_plan_item_rules_by_plan_id(plan.id)
+    #获取每个plan_item对应的plan_item_rule
+    #item_ids_to_rules 是一个dictionary，
+    #Key: plan_item.id
+    #Value: 对应的 plan_item_rules
     
-#     drug_ids = get_drug_ids_from_plan_items(plan_items)
-#     #获取plan_items中，drug_id对应的drug_name,
-#     #drug_ids_to_names 应该是一个dictionary
+    drug_ids = get_drug_ids_from_plan_items(plan_items)
+    #获取plan_items中，drug_id对应的drug_name,
+    #drug_ids_to_names 应该是一个dictionary
     
-#     drug_id_to_names = drug_repo.get_names_by_ids(drug_ids)
-#     #返回一个dictionary
-#     #key: drug_id
-#     #value: drug的name 注意，这里具体返回哪个name，或者是不是可以直接返回drug instances？有待讨论
+    drug_id_to_names = drug_repo.get_names_by_ids(drug_ids)
+    #返回一个dictionary
+    #key: drug_id
+    #value: drug的name 注意，这里具体返回哪个name，或者是不是可以直接返回drug instances？有待讨论
     
 
     
-#     #通过上面的5个方法，已经获取了所有需要的信息，开始算法部分
+    #通过上面的5个方法，已经获取了所有需要的信息，开始算法部分
     
-#     plan_items = fill_drug_names(drug_id_to_names)
+    plan_items = fill_drug_names(drug_id_to_names)
     
     
-#     plan_items = fill_date_and_time(plan_items, item_ids_to_rules)
-#     #这里设计一下，通过item_ids_to_rules generate出来加上重复后的plan_items，
-#     #并且每个plan_items需要带上date和time
-#     #顺便把plan_item的plan_item_rule帮订到plan_item上
+    plan_items = fill_date_and_time(plan_items, item_ids_to_rules)
+    #这里设计一下，通过item_ids_to_rules generate出来加上重复后的plan_items，
+    #并且每个plan_items需要带上date和time
+    #顺便把plan_item的plan_item_rule帮订到plan_item上
     
-#     plan.plan_items = plan_items
+    plan.plan_items = plan_items
     
-#     return plan
+    return plan
     
 #查看GetUserPlan的demo version，返回一些dummy data方便测试前端
 #查看plan,plan_item,plan_item_rules如何定义: repo/plan_repo.py
--- pagelogic/repo/plan_repo.py
+++ pagelogic/repo/plan_repo.py
@@ -97,7 +97,16 @@ def get_plan_by_id(plan_id): #return an instance of plan by plan_id
     print(result)
     cur.close()
     conn.close()
-    return result
+    plan_obj = Plan(
+        id=result["id"],
+        patient_id=result["patient_id"],
+        doctor_id=result["doctor_id"],
+        patient_name=result["patient_name"],
+        doctor_name=result["doctor_name"],
+        name=result["name"],
+        plan_items=None   
+    )
+    return plan_obj
 
 def get_all_plan_items_by_plan_id(plan_id): #return a list of plan_item
     conn = mydb()
@@ -109,16 +118,76 @@ def get_all_plan_items_by_plan_id(plan_id): #return a list of plan_item
     print(result)
     cur.close()
     conn.close()
-    return result
+
+    plan_item_list = []
+    for row in result:
+        item_obj = plan_item(
+            id=row["id"],
+            plan_id=row["plan_id"],
+            drug_id=row["drug_id"],
+            drug_name=row["drug_name"],
+            dosage=row["dosage"],
+            unit=row["unit"],
+            amount_literal=row["amount_literal"],
+            note=row["note"],
+            date=None,     # DB 查询不含 date → 固定为 None
+            time=None,     # DB 查询不含 time → 固定为 None
+            plan_item_rule=None  
+        )
+        plan_item_list.append(item_obj)
+    return plan_item_list
  
 def get_plan_item_rule_by_plan_item_id(plan_item_id): #return an instance of plan_item_rule
-    pass
+    conn = mydb()
+    cur = conn.cursor()
+    query = "select * from plan_item_rule where plan_item_id = %s"
+    cur.execute(query, (plan_item_id,))
+    result = cur.fetchall()   
+    cur.close()
+    conn.close()
+    return result
 
 #通过一个plan_id, 获取每个plan_item，以及其对应的plan_item_rule
 # 具体来说，获取plan_item_id to plan_item_rule 这样一个dictionary:
 #Key: plan_item.id
 #Value: 对应的 plan_item_rules
 def get_plan_item_rules_by_plan_id(plan_id):
-    item_id_to_rules = dict()
-
+    conn = mydb()
+    cur = conn.cursor()
+    query = """
+        SELECT pi.id AS plan_item_id, pir.*
+        FROM plan_item pi
+        LEFT JOIN plan_item_rule pir
+        ON pi.id = pir.plan_item_id
+        WHERE pi.plan_id = %s;
+    """
+    cur.execute(query, (plan_id,))
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    item_id_to_rules = {}
+    for row in rows:
+        item_id = row["plan_item_id"]
+        if row["id"] is not None:
+            rule_obj = plan_item_rule(
+            id=row["id"],
+            plan_item_id=row["plan_item_id"],
+            start_date=row["start_date"],
+            end_date=row["end_date"],
+            repeat_type=row["repeat_type"],
+            interval_value=row["interval_value"],
+            mon=row["mon"],
+            tue=row["tue"],
+            wed=row["wed"],
+            thu=row["thu"],
+            fri=row["fri"],
+            sat=row["sat"],
+            sun=row["sun"],
+            times=row["times"]
+        )
+        item_id_to_rules[item_id] = rule_obj
     return item_id_to_rules
+
+
+
+
