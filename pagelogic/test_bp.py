c3696f34cb8897e8377c2a0386da53cbe7052958
sky <skyjin@bu.edu>
2025-11-12 17:38:27 -0500

    add test blueprint and registered in app.py

---
+++ pagelogic/test_bp.py
@@ -0,0 +1,17 @@
+from flask import jsonify, render_template, Blueprint, request
+
+from pagelogic.service import plan_service
+
+test_bp = Blueprint('test_bp', __name__)
+
+
+@test_bp.route('/get_user_plan', methods=['GET'])
+def get_user():
+    user_id = request.args.get("id")
+    from_when = request.args.get("from")
+    to_when = request.args.get("to")
+
+    plan = plan_service.get_user_plan(user_id, from_when, to_when)
+    return jsonify({
+        plan
+    }), 200
